d28443feb50fd92b7cdab61f510eca0a7ed08ca3
David FletcherE <dtsfletcher@gmail.com>
2017-09-26 21:30:58 -0400

    can't get past first test

---
+++ changer.rb
@@ -0,0 +1,25 @@
+class Changer
+
+def initialize
+@@money = [25, 10, 5, 1]
+end
+
+
+def self.make_change(num)
+  @array = []
+  @array << num
+if num == @@money.each do |coin|
+  puts num
+else
+  n
+
+end
+
+
+
+
+
+
+
+
+end
